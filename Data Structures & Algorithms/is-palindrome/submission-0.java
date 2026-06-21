1d16802c014ef259fa04909d690502d3634fa156
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-06-21 23:57:28 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while(l <= r) {
+            while(l <= r && !alphaNums(s.charAt(l)))
+                l++;
+            while(l <= r && !alphaNums(s.charAt(r)))
+                r--;
+            
+            if(l <= r && Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+            
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    public boolean alphaNums(char c) {
+        return (
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            c >= '0' && c <= '9'
+        );
+    }
+}
