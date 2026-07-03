88b2b0b2ef77ae96fc6110f31a4beedc52893308
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-07-03 21:28:44 +0530

    Add: binary-tree-preorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-2.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null)    return res;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        TreeNode cur = root;
+
+        while(cur != null || !stack.isEmpty()) {
+            cur = stack.pop();
+            res.add(cur.val);
+
+            if(cur.right != null)
+                stack.push(cur.right);
+
+            if(cur.left != null)
+                stack.push(cur.left);
+            
+            cur = cur.left;
+        }
+        
+        return res;
+    }
+}
