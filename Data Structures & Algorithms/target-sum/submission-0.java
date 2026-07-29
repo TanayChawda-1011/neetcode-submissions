326de8940e146d84a6561c29f81e51d315d7cec1
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-07-30 00:44:26 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return backtrack(nums, 0, 0, target);
+    }
+
+    public int backtrack(int[] nums, int i, int total, int target) {
+        if(i == nums.length) {
+            return target == total ? 1 : 0;
+        }
+
+        return backtrack(nums, i + 1, total + nums[i], target) + backtrack(nums, i + 1, total - nums[i], target);
+    }
+}
