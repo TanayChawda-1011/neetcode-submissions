1db06595ac41d67e98d60fbfefd014bea55cf522
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-08-03 20:47:52 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0;
+
+        for(int i = 1; i <= nums.length; i++) {
+            xor1 ^= i;
+        }
+
+        int xor2 = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            xor2 ^= nums[i];
+        }
+
+        return xor1 ^ xor2;
+    }
+}
