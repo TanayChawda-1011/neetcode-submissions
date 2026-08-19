1b2f221f86fc8c34d5ce9dc5fad4273485ac18de
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-08-20 03:26:13 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        float threshold = nums.length / 2;
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+
+            if(map.get(n) > threshold)
+                return n;
+        }
+
+        return -1;
+    }
+}
