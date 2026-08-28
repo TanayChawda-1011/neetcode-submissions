acd6bebd5a99337327cc0bcab7d06b974dacdb88
Tanay <147864922+TanayChawda-1011@users.noreply.github.com>
2026-08-28 14:44:27 +0530

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int ROWS = grid.length, COLS = grid[0].length;
+        int res = 0;
+
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(grid[r][c] == '1') {
+                    dfs(grid, r, c);
+                    res++;                 
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public void dfs(char[][] grid, int r, int c) {
+        if(r < 0 || c < 0 || r == grid.length || c == grid[0].length || grid[r][c] == '0')
+            return;
+
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
